1d68e7011d6232ffa340281370fe4b7a8f4e7548
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-08-14 09:13:29 +0000

    imprved robustness if the result is poorly defined
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResultServer/trunk@15376 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/DbResultsServerTestCase.php
+++ test/DbResultsServerTestCase.php
@@ -12,31 +12,34 @@ class DbResultsServerTestCase extends UnitTestCase {
 
 	public function setUp(){		
 		TaoTestRunner::initTest();
-		$this->resultServer = new taoResultServer_models_classes_DbResultServer("SomeResult");
+		
+
 	}
-	public function testResultServer(){
-		$this->assertIsA($this->resultServer, 'taoResultServer_models_classes_DbResultServer');
-	}
-    public function testSetTestTaker() {
-        $this->resultServer->storeTestTaker('http://tao-dev/taodev.rdf#i1376035802900929');
-    }
-     public function testSetDelivery() {
-        $this->resultServer->storeDelivery('http://tao-dev/taodev.rdf#i1376035790164827');
-    }
-    public function testSetItemVariable(){
-        $outComeVariable = new taoResultServer_models_classes_OutcomeVariable();
+
+    public function testSetResult() {
+        $resultIdentifier = "atrial".rand(0,512);
+        $testTaker = 'http://tao-dev/taodev.rdf#i1376035802900929';
+        $delivery = 'http://tao-dev/taodev.rdf#i1376035790164827';
+        $test = 'http://tao-dev/taodev.rdf#i1376035966325031';
+        $item = "http://tao-dev/taodev.rdf#i1376035671745";
+        
+        $this->resultServer = new taoResultServer_models_classes_DbResultServer($resultIdentifier);
+        $this->assertIsA($this->resultServer, 'taoResultServer_models_classes_DbResultServer');
+        $this->resultServer->storeTestTaker($testTaker );
+        $this->resultServer->storeDelivery($delivery);
+         $outComeVariable = new taoResultServer_models_classes_OutcomeVariable();
         $outComeVariable->setBaseType("int");
         $outComeVariable->setCardinality("single");
         $outComeVariable->setIdentifier("Spatial representation");
         $outComeVariable->setValue("".rand(0,30));
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $outComeVariable, "xxx");
+        $this->resultServer->storeItemVariable($test, $item, $outComeVariable, "xxx");
 
         $outComeVariable = new taoResultServer_models_classes_OutcomeVariable();
         $outComeVariable->setBaseType("int");
         $outComeVariable->setCardinality("single");
         $outComeVariable->setIdentifier("Rotation in Space");
         $outComeVariable->setValue("".rand(0,50));
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $outComeVariable, "yyy");
+        $this->resultServer->storeItemVariable($test, $item, $outComeVariable, "yyy");
 
         $responseVariable = new taoResultServer_models_classes_ResponseVariable();
         $responseVariable->setBaseType("int");
@@ -44,7 +47,7 @@ class DbResultsServerTestCase extends UnitTestCase {
         $responseVariable->setIdentifier("historyResponse");
         $responseVariable->setCandidateResponse("choice_".rand(0,5));
         $responseVariable->setCorrectResponse(true);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
 
         //an unscored response
          $responseVariable = new taoResultServer_models_classes_ResponseVariable();
@@ -61,7 +64,7 @@ Qui te rend si hardi de troubler mon breuvage ?
 Dit cet animal plein de rage :
 Tu seras châtié de ta témérité.");
         $responseVariable->setCorrectResponse(true);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
 
 
         $responseVariable = new taoResultServer_models_classes_ResponseVariable();
@@ -70,8 +73,8 @@ Tu seras châtié de ta témérité.");
         $responseVariable->setIdentifier("populationResponse");
         $responseVariable->setCandidateResponse("choice_".rand(0,5));
         $responseVariable->setCorrectResponse(false);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
-        sleep(3);
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
         //4 different observations are submitted for the sam variableIdentifier
          $responseVariable = new taoResultServer_models_classes_ResponseVariable();
         $responseVariable->setBaseType("int");
@@ -79,31 +82,31 @@ Tu seras châtié de ta témérité.");
         $responseVariable->setIdentifier("planets");
         $responseVariable->setCandidateResponse("choice_1");
         $responseVariable->setCorrectResponse(false);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
-        sleep(2);
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
         $responseVariable = new taoResultServer_models_classes_ResponseVariable();
         $responseVariable->setBaseType("int");
         $responseVariable->setCardinality("single");
         $responseVariable->setIdentifier("planets");
         $responseVariable->setCandidateResponse("choice_3");
         $responseVariable->setCorrectResponse(false);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
-        sleep(2);
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
         $responseVariable = new taoResultServer_models_classes_ResponseVariable();
         $responseVariable->setBaseType("int");
         $responseVariable->setCardinality("single");
         $responseVariable->setIdentifier("planets");
         $responseVariable->setCandidateResponse("choice_5");
         $responseVariable->setCorrectResponse(true);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
-        sleep(2);
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
         $responseVariable = new taoResultServer_models_classes_ResponseVariable();
         $responseVariable->setBaseType("int");
         $responseVariable->setCardinality("single");
         $responseVariable->setIdentifier("planets");
         $responseVariable->setCandidateResponse("choice_4");
         $responseVariable->setCorrectResponse(false);
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $responseVariable, "yyy");
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
 
 
 
@@ -112,9 +115,63 @@ Tu seras châtié de ta témérité.");
         $traceVariable->setBaseType("string");
         $traceVariable->setCardinality("single");
         $traceVariable->setIdentifier("myTrace");
-        
+
         $traceVariable->setTrace("<xml><event id='5' domNode='xpath:/body/thiselement' eventName= 'Click' value=''</xml> ");
-        $this->resultServer->storeItemVariable("http://tao-dev/taodev.rdf#i1376035966325031", "http://tao-dev/taodev.rdf#i1376035671745", $traceVariable, "yyy");
+        $this->resultServer->storeItemVariable($test, $item, $traceVariable, "yyy");
+
+    }
+
+       public function testSetPoorlySpecifiedResult() {
+        $resultIdentifier = "loose".rand(0,512);
+        $testTaker = 'tt';
+        $delivery = 'del';
+        $test = 'test';
+        $item = "item";
+
+        $this->resultServer = new taoResultServer_models_classes_DbResultServer($resultIdentifier);
+        $this->assertIsA($this->resultServer, 'taoResultServer_models_classes_DbResultServer');
+        $this->resultServer->storeTestTaker($testTaker );
+        $this->resultServer->storeDelivery($delivery);
+         $outComeVariable = new taoResultServer_models_classes_OutcomeVariable();
+        $outComeVariable->setBaseType("int");
+        $outComeVariable->setCardinality("single");
+        $outComeVariable->setIdentifier("Spatial representation");
+        $outComeVariable->setValue("".rand(0,30));
+        $this->resultServer->storeItemVariable($test, $item, $outComeVariable, "xxx");
+
+        $outComeVariable = new taoResultServer_models_classes_OutcomeVariable();
+        $outComeVariable->setBaseType("int");
+        $outComeVariable->setCardinality("single");
+        $outComeVariable->setIdentifier("Rotation in Space");
+        $outComeVariable->setValue("".rand(0,50));
+        $this->resultServer->storeItemVariable($test, $item, $outComeVariable, "yyy");
+
+        $responseVariable = new taoResultServer_models_classes_ResponseVariable();
+        $responseVariable->setBaseType("int");
+        $responseVariable->setCardinality("single");
+        $responseVariable->setIdentifier("historyResponse");
+        $responseVariable->setCandidateResponse("choice_".rand(0,5));
+        $responseVariable->setCorrectResponse(true);
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
+        //an unscored response
+         $responseVariable = new taoResultServer_models_classes_ResponseVariable();
+        $responseVariable->setBaseType("int");
+        $responseVariable->setCardinality("single");
+        $responseVariable->setIdentifier("Dissertation");
+        $responseVariable->setCandidateResponse("²&é'(-è_çà');");
+        $responseVariable->setCorrectResponse(true);
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
+
+        $responseVariable = new taoResultServer_models_classes_ResponseVariable();
+        $responseVariable->setBaseType("int");
+        $responseVariable->setCardinality("single");
+        $responseVariable->setIdentifier("nocoorectresponse");
+        $responseVariable->setCandidateResponse("choice_".rand(0,5));
+
+        $this->resultServer->storeItemVariable($test, $item, $responseVariable, "yyy");
+
 
     }
 
