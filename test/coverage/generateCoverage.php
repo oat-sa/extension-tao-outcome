ad2da1272214efc5157abfa7cb9f31531cd5c577
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-17 13:47:52 +0000

    try to remove strick error from coverage
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResultServer/trunk@16239 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/coverage/generateCoverage.php
+++ test/coverage/generateCoverage.php
@@ -37,7 +37,7 @@ if(PHP_SAPI == 'cli'){
 else{
 	$reporter = new HtmlReporter();
 }
-
+error_reporting(0);
 require_once  PHPCOVERAGE_HOME. "/CoverageRecorder.php";
 require_once PHPCOVERAGE_HOME . "/reporter/HtmlCoverageReporter.php";
 //run the unit test suite
@@ -47,7 +47,9 @@ $covReporter = new HtmlCoverageReporter("Code Coverage Report taoResultServer", 
 $cov = new CoverageRecorder($includePaths, $excludePaths, $covReporter);
 //run the unit test suite
 $cov->startInstrumentation();
+error_reporting(E_ALL);
 $testSuite->run($reporter);
+error_reporting(0);
 $cov->stopInstrumentation();
 $cov->generateReport();
 $covReporter->printTextSummary(PHPCOVERAGE_REPORTS.'/taoResultServer_coverage.txt');
