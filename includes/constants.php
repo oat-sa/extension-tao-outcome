81462cb03e737cd93e8f561f9f1dffa2b85015fd
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-20 14:52:58 +0000

    tao default result server updated
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoResultServer/trunk@16419 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -22,10 +22,8 @@ $todefine = array(
     'TAO_RESULTSERVER_CLASS'	=> 'http://www.tao.lu/Ontologies/TAOResultServer.rdf#ResultServer',
     'TAO_RESULTSERVER_MODEL_PROP' =>'http://www.tao.lu/Ontologies/TAOResultServer.rdf#hasResultServerModel',
     'TAO_RESULTSERVER_MODEL_IMPL_PROP' =>'http://www.tao.lu/Ontologies/TAOResultServer.rdf#Implementation',
-    'TAO_DEFAULT_RESULT_SERVER' => 'http://www.tao.lu/Ontologies/TAOResultServer.rdf#taoResultServer',
-
+    'TAO_DELIVERY_DEFAULT_RESULT_SERVER' => 'http://www.tao.lu/Ontologies/TAOResult.rdf#taoResultServer',
     'TAO_VOID_RESULT_SERVER' => 'http://www.tao.lu/Ontologies/TAOResultServer.rdf#void',
-
     'PROPERTY_RESULTSERVER_ENDPOINT' => 'http://www.tao.lu/Ontologies/TAOResultServer.rdf#ResultServerEndpoint'
 );
 ?>
