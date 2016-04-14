bc53b30d08f8a419c9aa7fba09d5a924a2ba587d
cmoyon <camille@taotesting.com>
2016-04-14 17:28:40 +0200

    Working rest api

---
+++ actions/class.QtiRestResults.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+
+//http://tao.dev/taoResultServer/QtiRestResults?testtaker=http%3A%2F%2Ftao.local%2Fmytao.rdf%23i1460560178726251&delivery=http%3A%2F%2Ftao.local%2Fmytao.rdf%23i1460560209646275
+
+use oat\taoResultServer\models\classes\QtiResultsService;
+
+class taoResultServer_actions_QtiRestResults extends \tao_actions_CommonRestModule
+{
+    /**
+     * taoResultServer_actions_QtiRestResults constructor.
+     * Pass model service to handle http call business
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->service = QtiResultsService::singleton();
+    }
+
+    /**
+     * Override tao_actions_CommonRestModule::get to route only to getDeliveryExecution
+     *
+     * @param null $uri
+     * @return void
+     */
+    protected function get($uri = null){
+        try {
+            $this->service->setParameters($this->getParameters());
+            $data = $this->service->getDeliveryExecution();
+            $this->returnSuccess($data);
+        } catch (Exception $e) {
+            $this->returnFailure($e);
+        }
+    }
+
+    /**
+     * Optionnaly a specific rest controller may declare
+     * aliases for parameters used for the rest communication
+     */
+    protected function getParametersAliases()
+    {
+        return array(
+            "testtaker" => PROPERTY_DELVIERYEXECUTION_SUBJECT,
+            "delivery"  => PROPERTY_DELVIERYEXECUTION_DELIVERY
+        );
+    }
+
+    /**
+     * Optionnal Requirements for parameters to be sent on every service
+     */
+    protected function getParametersRequirements()
+    {
+        return array(
+            "get" => array(
+                PROPERTY_DELVIERYEXECUTION_SUBJECT,
+                PROPERTY_DELVIERYEXECUTION_DELIVERY
+            )
+        );
+    }
+
+}
