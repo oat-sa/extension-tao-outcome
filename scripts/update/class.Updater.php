c8238abffc35f05376186e21feef094ba7d01d13
Joel Bout <joel@taotesting.com>
2017-06-16 16:34:03 +0200

    Independant of generis version

--- scripts/update/class.Updater.php
+++ scripts/update/class.Updater.php
@@ -26,7 +26,7 @@ use oat\tao\model\accessControl\func\AclProxy;
 use oat\tao\model\accessControl\func\AccessRule;
 use oat\tao\model\user\TaoRoles;
 use oat\taoResultServer\models\classes\QtiResultsService;
-use oat\oatbox\config\ConfigurationService;
+use oat\taoResultServer\models\classes\ResultService;
 /**
  * 
  * @author Joel Bout <joel@taotesting.com>
@@ -58,8 +58,8 @@ class taoResultServer_scripts_update_Updater extends \common_ext_ExtensionUpdate
         $this->skip('2.12.0', '3.2.0');
         if ($this->isVersion('3.2.0')) {
             $service = $this->getServiceManager()->get(QtiResultsService::SERVICE_ID);
-            if ($service instanceof ConfigurationService) {
-                $this->getServiceManager()->register(QtiResultsService::SERVICE_ID, $service->getConfig());
+            if (!$service instanceof ResultService) {
+                $this->getServiceManager()->register(QtiResultsService::SERVICE_ID, new QtiResultsService());
             }
             $this->setVersion('3.2.1');
         }
