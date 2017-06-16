2234fdd6dafcd6546c7d6d6119e90c4ac8e7f88d
Joel Bout <joel@taotesting.com>
2017-06-16 15:35:28 +0200

    Ensure service update

--- scripts/update/class.Updater.php
+++ scripts/update/class.Updater.php
@@ -26,6 +26,7 @@ use oat\tao\model\accessControl\func\AclProxy;
 use oat\tao\model\accessControl\func\AccessRule;
 use oat\tao\model\user\TaoRoles;
 use oat\taoResultServer\models\classes\QtiResultsService;
+use oat\oatbox\config\ConfigurationService;
 /**
  * 
  * @author Joel Bout <joel@taotesting.com>
@@ -54,6 +55,13 @@ class taoResultServer_scripts_update_Updater extends \common_ext_ExtensionUpdate
             $this->setVersion('2.12.0');
         }
 
-        $this->skip('2.12.0', '3.2.1');
+        $this->skip('2.12.0', '3.2.0');
+        if ($this->isVersion('3.2.0')) {
+            $service = $this->getServiceManager()->get(QtiResultsService::SERVICE_ID);
+            if ($service instanceof ConfigurationService) {
+                $this->getServiceManager()->register(QtiResultsService::SERVICE_ID, $service->getConfig());
+            }
+            $this->setVersion('3.2.1');
+        }
     }
 }
