d0c17daecdeba5b318788aecac59c766c7caaeeb
Razhkou <yuryr@taotesting.com>
2019-10-10 12:30:50 +0300

    TTD-18 Added\changed 'generis' version to '>=12.5.0' in `requires` secition

--- manifest.php
+++ manifest.php
@@ -17,6 +17,7 @@ return array(
     'author' => 'Open Assessment Technologies',
     //taoResults may be needed for the taoResults taoResultServerModel that uses taoResults db storage
 	'requires' => array(
+        'generis' => '>=12.5.0',
         'tao' => '>=27.2.0'
 	),
 	'models' => array(
