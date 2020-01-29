9ae45ac35c88e57e4292cf407c0f4cdc2c887c44
Camille Moyon <camille@taotesting.com>
2020-01-29 11:07:45 +0100

    Bump version

--- scripts/update/class.Updater.php
+++ scripts/update/class.Updater.php
@@ -80,6 +80,6 @@ class taoResultServer_scripts_update_Updater extends \common_ext_ExtensionUpdate
             $this->setVersion('5.1.0');
         }
 
-        $this->skip('5.1.0', '10.2.1');
+        $this->skip('5.1.0', '10.2.2');
     }
 }
