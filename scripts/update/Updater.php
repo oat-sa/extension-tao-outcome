07ccec029166483e0b392620ae1d972c167edfee
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-09-28 15:44:30 +0200

    TBD-63 chore(version): bump a hotfix one

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -88,7 +88,7 @@ class Updater extends common_ext_ExtensionUpdater
         }
 
         $this->skip('12.0.0', '12.1.1');
-        
+
         //Updater files are deprecated. Please use migrations.
         //See: https://github.com/oat-sa/generis/wiki/Tao-Update-Process
 
