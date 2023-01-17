bb6d91bea0cb61d2993b8ec73a5389b594b8aa7f
Nikita Pimenov <nikita.pimenov@taotesting.com>
2023-01-17 16:50:57 +0400

    feat: event for sending AGS scores on demand

---
+++ models/Events/DeliveryExecutionResultsRecalculated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace oat\taoResultServer\models\Events;
+
+use oat\oatbox\event\Event;
+use oat\taoDelivery\model\execution\DeliveryExecutionInterface;
+
+class DeliveryExecutionResultsRecalculated implements Event
+{
+    private DeliveryExecutionInterface $deliveryExecution;
+    private ?float $totalScore;
+    private ?float $totalMaxScore;
+
+    public function __construct(DeliveryExecutionInterface $deliveryExecution, ?float $totalScore, ?float $totalMaxScore)
+    {
+        $this->deliveryExecution = $deliveryExecution;
+        $this->totalScore = $totalScore;
+        $this->totalMaxScore = $totalMaxScore;
+    }
+
+    public function getDeliveryExecution(): DeliveryExecutionInterface
+    {
+        return $this->deliveryExecution;
+    }
+
+    public function getName(): string
+    {
+        return __CLASS__;
+    }
+
+    public function getScore(): ?float
+    {
+        return $this->totalScore;
+    }
+
+    public function getMaxScore(): ?float
+    {
+        return $this->totalMaxScore;
+    }
+}
